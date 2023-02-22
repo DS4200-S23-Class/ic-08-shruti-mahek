e3508a09da085b3b6855c4c54b95599d215790bd
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-02-22 14:35:55 -0500

    Update main.js

--- d3.v6.1.1/main.js
+++ d3.v6.1.1/main.js
@@ -8,6 +8,37 @@ const VIS_WIDTH = FRAME_WIDTH - MARGINS.left -MARGINS.right;
 
 const FRAME = d3.select("#vis")
                     .append("svg")
-                        .attr("height", FRAME_HEIGHT)
-                        .attr("width", FRAME_WIDTH)
+                        .attr("height", HEIGHT)
+                        .attr("width", WIDTH)
                         .attr("class", "frame");
+
+function plot(){
+    d3.csv("data/data.csv").then((data) => { 
+
+        const Y_MAX = d3.max(data, (d) => { return parseInt(d.amount); });
+        
+        const X_SCALE = d3.scaleBand()
+            .range([0, VIS_WIDTH])
+            .domain(data.map(function(d) {return d.category;}));
+            
+        const Y_SCALE = d3.scaleLinear() 
+            .domain([0, Y_MAX])
+            .range([VIS_HEIGHT, MARGINS.top]);
+      
+      
+      FRAME.selectAll('bars')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr("x", (d) => { return X_SCALE(d.category) + MARGINS.left; })
+            .attr("y", (d) => { return Y_SCALE(d.amount) + MARGINS.bottom; })
+              .attr('width', X_SCALE.bandwidth())
+              .attr('height', (d) => VIS_HEIGHT - Y_SCALE(d.amount))
+              .attr('fill', 'powderblue')
+              .attr('class', 'bar');
+      
+       });
+};
+
+
+plot()
